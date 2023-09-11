133db30270d78e6255b373a29dcc60366cbae836
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-11 07:41:26 +0300

    half relation

--- src/spravochniki/admin.py
+++ src/spravochniki/admin.py
@@ -4,3 +4,5 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Book)
+admin.site.register(models.Author)
+admin.site.register(models.Country)
--- src/spravochniki/models.py
+++ src/spravochniki/models.py
@@ -2,7 +2,41 @@ from django.db import models
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(
+        verbose_name="Country",
+        max_length=20
+    )
+    def __str__(self):
+        return str(self.pk) + " " + self.name
+
+class Author(models.Model):
+    country = models.ForeignKey(
+        "spravochniki.Country",
+        on_delete=models.PROTECT,
+        verbose_name="Country",
+        default=1,
+        related_name="authors"
+    )
+    name = models.CharField(
+        verbose_name="Author name",
+        max_length=20
+    )
+    description = models.TextField(
+        verbose_name="Author description",
+        null=True,
+        blank=True
+    )
+    def __str__(self):
+        return str(self.pk) + " " + self.name
+
 class Book(models.Model):
+    author = models.ForeignKey(
+        "spravochniki.Author",
+        on_delete=models.PROTECT,
+        verbose_name="Author",
+        related_name="books"
+    )
     name = models.CharField(
         verbose_name="Book name",
         max_length=20
